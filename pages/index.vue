47913d89c938c6d788a960c674903309c7d8c4d0
Logan Connolly <logan.connolly@protonmail.com>
2020-12-29 18:31:06 +0100

    Add buttons directly on page

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,14 @@
       Keep your vehicles on the road
       <span class="welcome-subtitle">where they belong</span>
     </div>
+    <div class="buttons">
+      <nuxt-link to="services" class="button button__primary"
+        >Our Services</nuxt-link
+      >
+      <nuxt-link to="contact" class="button button__secondary"
+        >Contact Us</nuxt-link
+      >
+    </div>
   </div>
 </template>
 
@@ -14,8 +22,11 @@ export default {};
 <style scoped>
 .welcome {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 2em;
+  text-align: center;
 }
 
 .welcome-title {
@@ -34,6 +45,50 @@ export default {};
   .welcome-subtitle {
     font-size: 1.75rem;
     font-weight: 700;
+  }
+}
+
+.buttons {
+  display: inline-flex;
+  justify-content: start;
+  gap: 0.5em;
+}
+
+.button {
+  display: inline-block;
+  padding: 0.46em 1.6em;
+  border: 0.1em solid #000000;
+  margin: 0 0.2em 0.2em 0;
+  border-radius: 0.12em;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-weight: 300;
+  color: #000000;
+  text-shadow: 0 0.04em 0.04em rgba(0, 0, 0, 0.35);
+  background-color: #ffffff;
+  text-align: center;
+  transition: all 0.15s;
+}
+
+.button:hover {
+  text-shadow: 0 0 2em rgba(255, 255, 255, 1);
+}
+
+.button__primary {
+  background-color: #13478e;
+  color: #fff;
+}
+
+.button__secondary {
+  border-color: #13478e;
+  background-color: rgba(0, 0, 0, 0);
+  color: #13478e;
+}
+
+@media all and (max-width: 30em) {
+  .button {
+    display: block;
+    margin: 0.4em auto;
   }
 }
 </style>
