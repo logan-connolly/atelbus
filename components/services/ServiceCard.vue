f23c3521dc0f822fd2507ccb3a8b87355728d359
Logan Connolly <logan.connolly@protonmail.com>
2021-01-23 12:59:48 +0100

    Add first draft of service card overlay

--- components/services/ServiceCard.vue
+++ components/services/ServiceCard.vue
@@ -1,12 +1,20 @@
 <template>
-  <nuxt-link :to="service.link">
-    <nuxt-image
-      class="service-image"
-      :lazy="false"
-      sizes="200,340:350,700:300,1000:350,1200:400"
-      :src="service.img"
-    />
-  </nuxt-link>
+  <div class="service-card">
+    <nuxt-link :to="service.link">
+      <nuxt-image
+        :src="service.img"
+        :lazy="false"
+        class="service-image"
+        fit="cover"
+        sizes="200,340:350,700:300,1000:350,1200:400"
+      />
+      <div class="service-overlay">
+        <div class="service-overlay__text">
+          {{ service.name }}
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
@@ -16,8 +24,48 @@ export default {
 </script>
 
 <style>
+.service-card {
+  position: relative;
+}
 .service-image {
   border-radius: 5%;
   box-shadow: 5px 5px 5px #999;
+}
+.service-overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: 0.5s ease;
+  border-radius: 5%;
+  background-color: #13478e;
+}
+.service-card .service-overlay {
+  opacity: 0.6;
+}
+.service-card:hover .service-overlay {
+  opacity: 0.9;
+}
+.service-card:hover .service-overlay {
+  box-shadow: 10px 10px 10px #999;
+}
+.service-card:hover .service-overlay__text {
+  font-weight: bold;
+  opacity: 1;
+}
+.service-overlay__text {
+  color: white;
+  font-size: 24px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
 }
 </style>
