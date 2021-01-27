068bf3e128406748ff37808511afcb0a78d3b443
Logan Connolly <logan.connolly@protonmail.com>
2021-01-27 19:19:41 +0100

    Substitute contact form with only contact information

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,25 +1,88 @@
 <template>
   <page-layout name="Contact Us">
     <template v-slot:header>
-      Get in touch with us by filling out the contact form.
+      Get in touch with us immediately via phone or email. Or simply stop by and
+      visit us!
     </template>
     <template v-slot:content>
-      <contact-form></contact-form>
+      <div class="contact-items">
+        <div class="contact-item contact-item__phone">
+          <a href="tel: +1 (301) 210-5100">
+            <fa :icon="['fas', 'phone-square-alt']" />
+            (301) 210-5100
+          </a>
+        </div>
+        <div class="contact-item contact-item__email">
+          <a href="mailto:info@atelbus.com">
+            <fa :icon="['fas', 'envelope-square']" />
+            info@atelbus.com
+          </a>
+        </div>
+        <div class="contact-item contact-item__address">
+          <a href="https://g.page/ATELBusandTruck?share">
+            <fa :icon="['fas', 'map-marker-alt']" />
+            12120 Conway Rd, Beltsville, MD 20705
+          </a>
+        </div>
+      </div>
+      <div class="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12392.555081894823!2d-76.8861638!3d39.0577516!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd27dd8b154634017!2sATEL%20Bus%20and%20Truck%20Service%20Center%2C%20Inc.!5e0!3m2!1sen!2sde!4v1611768520264!5m2!1sen!2sde"
+          width="600"
+          height="450"
+          frameborder="0"
+          style="border:0;"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </div>
     </template>
   </page-layout>
 </template>
 
 <script>
-import ContactForm from "@/components/contact/ContactForm.vue";
-export default {
-  components: {
-    ContactForm
-  }
-};
+export default {};
 </script>
 
 <style scoped>
-.contact-form {
-  color: red;
+.contact-items {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.contact-item {
+  padding-left: 2rem;
+  padding-bottom: 1.2rem;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+a:hover {
+  color: #13478e;
+}
+
+a svg {
+  color: #13478e;
+  font-size: 1.5rem;
+}
+
+.map {
+  margin-top: 2rem;
+}
+
+@media screen and (max-width: 768px) {
+  .contact-items {
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: start;
+  }
+  .map {
+    display: none;
+  }
 }
 </style>
