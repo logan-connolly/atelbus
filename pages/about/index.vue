47b76f7d822a7696431ab29e66d72528db0cdf79
Logan Connolly <logan.connolly@protonmail.com>
2023-03-11 19:20:26 +0100

    feat(pages): update the `about` page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,15 +2,33 @@
   <page-layout name="About">
     <template v-slot:header>
       <p>
-        ATEL Bus & Truck is a family-owned business. Established in 1994, we
-        have over 25 years of experience in motor repairs and services. We
-        provide high quality, reliable and comprehensive bus, truck and RV
-        services for the public and private sector.
+        Welcome to ATEL, our family-owned company that has been providing
+        top-quality service for buses, trucks, and RVs since 1998. Our passion
+        for the industry is evident in every service we provide.
       </p>
       <p>
-        We service bus, fleets, recreational vehicles (RV), motorhomes, cars,
-        sport utility vehicle (SUV) and trucks in the Washington D.C. and
-        Baltimore areas. We accept all insurance policies.
+        At ATEL, we take great pride in our expertise, commitment, and
+        outstanding customer service. Our team of certified technicians has
+        years of experience and training, allowing us to deliver the best
+        service possible to our clients.
+      </p>
+      <p>
+        We understand that a breakdown or maintenance issue with your vehicle
+        can be a major inconvenience, which is why we offer timely and efficient
+        repairs to get you back on the road as soon as possible. Our services
+        include routine maintenance, diagnostic testing, brake repairs, engine
+        repairs, transmission service, and more.
+      </p>
+      <p>
+        We use only the highest quality parts and equipment to ensure the
+        longevity and reliability of your vehicle. Our team is dedicated to
+        providing you with personalized service that meets your unique needs and
+        exceeds your expectations.
+      </p>
+      <p>
+        Thank you for considering ATEL for your vehicle service needs. We look
+        forward to earning your trust and becoming your go-to service provider
+        for years to come.
       </p>
     </template>
     <template v-slot:content> </template>
