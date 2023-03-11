02e802988f4ef5f04176bb308dcc624afeeb2807
Logan Connolly <logan.connolly@protonmail.com>
2023-03-11 23:02:10 +0100

    refactor: only load in icons that are used

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,14 +1,32 @@
 import Vue from "vue";
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhoneSquareAlt,
+  faEnvelope,
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faLinkedin,
+  faYelp,
+} from "@fortawesome/free-brands-svg-icons";
 
 // This is important, we are going to let Nuxt.js worry about the CSS
 config.autoAddCss = false;
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas);
+library.add(
+  faPhoneSquareAlt,
+  faEnvelope,
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faFacebook,
+  faLinkedin,
+  faYelp
+);
 
 // Register the component globally
 Vue.component("font-awesome-icon", FontAwesomeIcon);
